0884c1870c0d15c4072fa6163213421f93d59026
Trinhtrinh18 <151738716+Trinhtrinh18@users.noreply.github.com>
2024-06-14 23:59:25 +0700

    Update Quanlykhoahoc.h

--- Quanlykhoahoc.h
+++ Quanlykhoahoc.h
@@ -4,12 +4,15 @@ struct User {
     string password[20];
     string email[20];
     string fullName[20];
+    string role[10];
 };
 
 // Cấu trúc Sinh viên (Student)
 struct Student {
     string studentId[10];
-    string fullName[20];
-    string email[20];
-    string phoneNumber[10];
+    string firstName[20];
+    string lastName[20];
+    string gender[3];
+    string dateOfBirth;
+    string socialId[10];
 };
