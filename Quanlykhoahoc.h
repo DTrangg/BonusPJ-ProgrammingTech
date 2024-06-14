89559e73fbce06e6f8d937b0cf5a19eb03ee9de2
Trinhtrinh18 <151738716+Trinhtrinh18@users.noreply.github.com>
2024-06-14 19:12:29 +0700

    Create Quanlykhoahoc.h

---
+++ Quanlykhoahoc.h
@@ -0,0 +1,15 @@
+// Cấu trúc User
+struct User {
+    string username;
+    string password;
+    string email;
+    string fullName;
+};
+
+// Cấu trúc Sinh viên (Student)
+struct Student {
+    string studentId;
+    string fullName;
+    string email;
+    string phoneNumber;
+};
