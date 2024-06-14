1b4bdd6ff4dc0474f887c1001f5d34a4c84211d1
Brr~ <152635141+DTrangg@users.noreply.github.com>
2024-06-14 19:58:09 +0700

    Add files via upload

---
+++ Struct Course + Score.cpp
@@ -0,0 +1,22 @@
+// Struct Course
+struct Course{
+	int ID;
+	string Name;
+	string ClassName;
+	string Teacher;
+	int NumOfCredits;
+	int Size;
+	string InDay;
+	int Sesion;	
+};
+
+// Struct Score
+struct Score{
+	int No;
+	string StudentID;
+	string StudentName;
+	double Total;
+	double Final;
+	double Midterm;
+	double Other;
+};
